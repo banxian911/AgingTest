bb02c58b9318ccf6e02fa136eccb6d7c438fa152
banxian911 <172647099@qq.com>
2016-09-20 18:30:40 +0800

    增加Apk处理工具类

---
+++ src/com/sprocomm/utils/ApkRunInstallUtil.java
@@ -0,0 +1,127 @@
+package com.sprocomm.utils;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ApkRunInstallUtil {
+	
+	private static final String TAG = "InstallPlay3DApp";
+	private static final String MTAG = "AgingTest";
+	
+	public ApkRunInstallUtil(Context context){
+		
+	}
+	
+	/**
+     * 启动App
+     * @param context
+     */
+    public static void RunApp(Context context,String packageName) {
+    	
+    	Log.d(MTAG, TAG + "---RunApp start---");
+       context.startActivity(context.getPackageManager().getLaunchIntentForPackage(packageName));
+    }
+
+    /**
+     * 检测某个应用是否安装
+     * 
+     * @param context
+     * @param packageName
+     * @return
+     */
+    public static boolean isAppInstalled(Context context, String packageName) {
+    	Log.d(MTAG, TAG + "---isAppInstalled packageName---" +packageName );
+        try {
+            context.getPackageManager().getPackageInfo(packageName, 0);
+            return true;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
+   
+    /** 
+     * 获取文件安装的Intent 
+     *  
+     * @param file 
+     * @return 
+     */  
+    public static void MakeApk(Context context,String packagePath) {  
+       // Uri uri = Uri.fromFile(file);  
+    	Uri uri = Uri.parse(packagePath);
+        String type = "application/vnd.android.package-archive";  
+        Intent intent = new Intent("android.intent.action.VIEW");  
+        intent.addCategory("android.intent.category.DEFAULT");  
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  
+        intent.setDataAndType(uri, type);  
+        context.startActivity(intent); 
+    }  
+    
+   
+    /**
+    *强制停止应用程序
+    * @param pkgName
+    * 需要添加---android:sharedUserId="android.uid.system"
+    * <uses-permission android:name="android.permission.FORCE_STOP_PACKAGES"/>
+    * 
+    */
+	public static void forceStopPackage(Context context, String pkgName) throws Exception {
+		Log.d(MTAG, TAG + "---forceStopPackage---");
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		Method method = Class.forName("android.app.ActivityManager").getMethod("forceStopPackage", String.class);
+		method.invoke(am, pkgName);
+	}
+    
+    /** 
+     * 判断应用是否正在运行 
+     *  
+     * @param context 
+     * @param packageName 
+     * @return 
+     */  
+	public static boolean isRunning(Context context, String packageName) {
+		Log.d(MTAG, TAG + "--- isRunning---");
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		List<RunningAppProcessInfo> list = am.getRunningAppProcesses();
+		for (RunningAppProcessInfo appProcess : list) {
+			String processName = appProcess.processName;
+			if (processName != null && processName.equals(packageName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	 /** 
+     * 判断应用是否已安装 
+     *  
+     * @param context 
+     * @param packageName 
+     * @return 
+     */  
+    private static boolean isInstalled(Context context, String packageName) {  
+        boolean hasInstalled = false;  
+        PackageManager pm = context.getPackageManager();  
+        List<PackageInfo> list = pm  
+                .getInstalledPackages(PackageManager.PERMISSION_GRANTED);  
+        for (PackageInfo p : list) {  
+            if (packageName != null && packageName.equals(p.packageName)) {  
+                hasInstalled = true;  
+                break;  
+            }  
+        }  
+        return hasInstalled;  
+    }  
+
+}
